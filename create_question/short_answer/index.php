af8e5a2482c867a5581127511dcb42e4e8d8c90e
ilyalang <ilyalang95@gmail.com>
2019-01-28 13:48:32 +0300

    Create index.php

---
+++ create_question/short_answer/index.php
@@ -0,0 +1,45 @@
+<?php 
+
+
+
+?>
+
+
+<html> 
+	<head>
+			<title>Вопросник</title>
+			<meta lang="ru">
+			<link rel="stylesheet" type="text/css" href="style.css">
+	 </head>
+		<body> 
+			<div class="header">
+
+
+
+			</div>
+			
+				<div class="flex">
+    			<div class="itm itm1"><a href="#">Мои Вопросы</div>
+    			<div class="itm itm2"><a href="create_question">Создать вопрос</a></div>
+    			<div class="itm itm3"><a href="create_test">Создать тест</a></div>
+    			<div class="itm itm4">Экспорт вопросов</div>
+			</div>
+
+				<div> 
+					<h2>Создание вопроса с коротким ответом</h2>
+
+					<form> 
+						<p>Введите текст вопроса<p>
+							<textarea></textarea>
+
+						<p><input type="submit"></p>
+					</form>
+
+				</div>
+
+
+		</body>
+
+
+
+</html>
