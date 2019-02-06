ac2ae5e0e0bf7147bb4ece9f5c5f2478a644978d
ilyalang <ilyalang95@gmail.com>
2019-02-06 19:46:11 +0300

    06.02.2019
    Подключение бд к веб-приложению

--- create_question/short_answer/index.php
+++ create_question/short_answer/index.php
@@ -1,8 +1,10 @@
 <?php 
-
-
-
+	$mysqli = new mysqli('localhost', 'root','', 'diplom');
+	if (mysqli_connect_errno()) {
+		echo "Подключение невозможно: ".mysqli_connect_error();
+	}
 ?>
+
 
 
 <html> 
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php 
-
-
-
+	$mysqli = new mysqli('localhost', 'root','', 'diplom');
+	if (mysqli_connect_errno()) {
+		echo "Подключение невозможно: ".mysqli_connect_error();
+	}
 ?>
 
 
