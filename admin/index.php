899f951e855be2e47cabc57780a1f3307780a5c4
ilyalang <ilyalang95@gmail.com>
2019-02-13 00:39:56 +0300

    13,02,2019

--- admin/index.php
+++ admin/index.php
@@ -1,52 +1,43 @@
-<?php 
-	$mysqli = new mysqli('localhost', 'root','', 'diplom');
-	if (mysqli_connect_errno()) {
-		echo "Подключение невозможно: ".mysqli_connect_error();
-	}
+<?php
+	$mysqli = new mysqli('localhost', 'root','', 'voprosnik');
 ?>
 
 
-<html> 
+<html>
 	<head>
 			<title>Вопросник</title>
 			<meta lang="ru">
 			<link rel="stylesheet" type="text/css" href="../style.css">
 	 </head>
-	 <body> 
+	 <body>
 	 	<div class="header">
 				<img src="../logo.png"> <h4>Административная панель</h4>
 				</div>
 			<div class="tabs">
     <input id="tab1" type="radio" name="tabs" checked>
     <label for="tab1" title="questions">Вопросы</label>
- 
+
     <input id="tab2" type="radio" name="tabs">
     <label for="tab2" title="types">Типы вопросов</label>
 
-    <input id="tab3" type="radio" name="tabs">
-    <label for="tab3" title="pred_obl">Предметные области</label>
- 
- 
     <section id="content-tab1">
         <p>
 		Здесь отобрадаются вопросы пользователей
         </p>
-    </section>  
+    </section>
     <section id="content-tab2">
-        <p>
-         Здесь отображаются типы вопросов 
-        </p>ааавававаааваавааааавса
-    </section>    
-    <section id="content-tab3">
-        <p>
-         Здесь отображаются предметные области </p>
-         
+			<?php
+				$result = mysqli_query($mysqli, "SELECT * FROM pred_ob");
+				while ($row = mysqli_fetch_assoc($result)){
+					echo $row['name_obl'];
+				}
+		?>
          <a href="">Добавить предметную область</a>
-         <form> 
+         <form>
          	<p>Введите название предметной области: <input type="text">  <input type="submit" value="Добавить"> </p>
          </form>
         </p>
-    </section> 
+    </section>
 </div>
 
 
